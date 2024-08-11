0c17979e9e19c39dd1c72defe0e37b3159668db8
Markus Mauch <markus.mauch@gmail.com>
2024-08-11 09:14:28 +0000

    update entity id

--- src/Devices/LichtschalterSchlafzimmer.ts
+++ src/Devices/LichtschalterSchlafzimmer.ts
@@ -1,4 +1,4 @@
 import {Dimmer} from "../Library/Dimmer";
 
-new Dimmer("light.licht", "input:0");
+new Dimmer("light.licht_schlafzimmer", "input:0");
 new Dimmer("light.hintergrundlicht_schlafzimmer", "input:1");
